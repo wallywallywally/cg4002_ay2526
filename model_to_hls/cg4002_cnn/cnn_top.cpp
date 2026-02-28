7f35d09565eeb43e8d1b7899c54b6ab3c30b9016
wallywallywally <willsonhanzhekai@gmail.com>
2026-02-28 18:46:08 +0800

    Update top

--- model_to_hls/cg4002_cnn/cnn_top.cpp
+++ model_to_hls/cg4002_cnn/cnn_top.cpp
@@ -13,39 +13,45 @@ void cnn_top(
 
     // Copy to local memory
     data_t local_in[IN_CH][IN_LEN];
-    #pragma HLS ARRAY_RESHAPE variable=local_in cyclic factor=2 dim=1
+    // #pragma HLS ARRAY_RESHAPE variable=local_in cyclic factor=2 dim=1
+    #pragma HLS BIND_STORAGE variable=local_in type=ram_2p impl=bram
 
     for(int ic=0; ic<IN_CH; ic++) {
         for(int j=0; j<IN_LEN; j++) {
-            #pragma HLS PIPELINE
+            #pragma HLS PIPELINE II=1
             local_in[ic][j] = input[ic][j];
         }
     }
     
     // Internal Buffers for Layer Outputs
     static data_t layer1_out[L1_OUT_CH][L1_WINDOW];
-    #pragma HLS ARRAY_RESHAPE variable=layer1_out cyclic factor=2 dim=1
+    // #pragma HLS ARRAY_RESHAPE variable=layer1_out cyclic factor=2 dim=1
     static data_t layer2_out[L2_OUT_CH][L2_WINDOW];
-    #pragma HLS ARRAY_RESHAPE variable=layer2_out cyclic factor=2 dim=1
+    // #pragma HLS ARRAY_RESHAPE variable=layer2_out cyclic factor=2 dim=1
     static data_t pool_out[L2_OUT_CH];
 
     #pragma HLS BIND_STORAGE variable=conv_block_0_weight type=rom_2p impl=bram
-    #pragma HLS BIND_STORAGE variable=conv_block_2_weight type=rom_2p impl=bram
+    #pragma HLS BIND_STORAGE variable=conv_block_3_weight type=rom_2p impl=bram
+
 
     // --- LAYER 1: Conv1d(8, 32, k=3) ---
     L1_Conv: for(int oc=0; oc < L1_OUT_CH; oc++) {
         for(int i=0; i < L1_WINDOW; i++) {
             #pragma HLS PIPELINE II=8
-            ap_fixed<32, 12> sum = conv_block_0_bias[oc];
+            #pragma HLS ALLOCATION operation instances=mul limit=4
+            #pragma BIND_OP variable=sum op=add impl=dsp
+            data_t sum = conv_block_0_bias[oc];
             for(int ic=0; ic < IN_CH; ic++) {
                 for(int k=0; k < KERNEL_SIZE_1; k++) {
                     int idx = (oc * IN_CH * KERNEL_SIZE_1) + (ic * KERNEL_SIZE_1) + k;
-                    sum += (ap_fixed<32,12>)local_in[ic][i+k] * (ap_fixed<32,12>)conv_block_0_weight[idx];
+                    sum += local_in[ic][i+k] * conv_block_0_weight[idx];
                 }
             }
             
             // LeakyReLU
-            layer1_out[oc][i] = (sum > 0) ? (data_t)sum : (data_t)(sum * 0.1);
+            data_t leaky;
+            leaky = (sum >> 4) + (sum >> 5);
+            layer1_out[oc][i] = (sum > 0) ? sum : leaky;
         }
     }
 
@@ -53,35 +59,39 @@ void cnn_top(
     L2_Conv: for(int oc=0; oc < L2_OUT_CH; oc++) {
         for(int i=0; i < L2_WINDOW; i++) {
             #pragma HLS PIPELINE II=8
-            ap_fixed<32, 12> sum = conv_block_2_bias[oc];
+            #pragma HLS ALLOCATION operation instances=mul limit=8
+            #pragma BIND_OP variable=sum op=add impl=dsp
+            data_t sum = conv_block_3_bias[oc];
             for(int ic=0; ic < L1_OUT_CH; ic++) {
                 for(int k=0; k < KERNEL_SIZE_2; k++) {
                     int idx = (oc * L1_OUT_CH * KERNEL_SIZE_2) + (ic * KERNEL_SIZE_2) + k;
-                    sum += (ap_fixed<32,12>)layer1_out[ic][i+k] * (ap_fixed<32,12>)conv_block_2_weight[idx];
+                    sum += layer1_out[ic][i+k] * conv_block_3_weight[idx];
                 }
             }
             
-            layer2_out[oc][i] = (sum > 0) ? (data_t)sum : (data_t)(sum * 0.1);
+            data_t leaky;
+            leaky = (sum >> 4) + (sum >> 5);
+            layer2_out[oc][i] = (sum > 0) ? sum : leaky;
         }
     }
 
     // --- LAYER 3: AdaptiveAvgPool1d(1) ---
     Pool: for(int c=0; c < L2_OUT_CH; c++) {
-        #pragma HLS PIPELINE
-        ap_fixed<32, 12> acc = 0;
+        #pragma HLS PIPELINE II=2
+        data_t acc = 0;
         for(int i=0; i < L2_WINDOW; i++) {
             acc += layer2_out[c][i];
         }
-        pool_out[c] = (data_t)(acc / L2_WINDOW);
+        pool_out[c] = acc / (data_t)L2_WINDOW;
     }
 
     // --- LAYER 4: Fully Connected (64 -> 8) ---
     FC: for(int cl=0; cl < NUM_CLASSES; cl++) {
-        #pragma HLS PIPELINE
-        ap_fixed<32, 12> sum = fc_bias[cl];
+        #pragma HLS PIPELINE II=2
+        data_t sum = fc_bias[cl];
         for(int i=0; i < L2_OUT_CH; i++) {
             sum += pool_out[i] * fc_weight[(cl * L2_OUT_CH) + i];
         }
-        output[cl] = (data_t)sum;
+        output[cl] = sum;
     }
 }
